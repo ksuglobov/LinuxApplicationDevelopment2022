c5e81cfee39002914dd684552077f64b4a910991
ksuglobov <ksuglobov@outlook.com>
2022-09-23 14:08:45 +0300

    Fixed memory leak and handled allocation error

--- 01_GitBuildReq/hello.c
+++ 01_GitBuildReq/hello.c
@@ -6,15 +6,23 @@ main(void)
 {
     size_t str_len = 3;
     char *buffer = (char *) malloc((str_len + 1) * sizeof(*buffer));
+    if (buffer == NULL){
+        fprintf(stderr, "Cannot allocate memory!\n");
+        return 1;
+    }
 
     size_t i = 0;
     char c = 0;
 
     scanf("%c", &c);
-    while (c > 0 && c != '\n') {
-        if (i == str_len) {
+    while (c > 0 && c != '\n'){
+        if (i == str_len){
             str_len *= 2;
             buffer = (char *) realloc(buffer, (str_len + 1) * sizeof(*buffer));
+            if (buffer == NULL){
+                fprintf(stderr, "Cannot allocate memory!\n");
+                return 1;
+            }
         }
         buffer[i++] = c; 
         scanf("%c", &c);
@@ -23,6 +31,8 @@ main(void)
 
 //    printf("[%s], %ld \n", buffer, str_len);
     printf("Hello, %s!\n", buffer);
+
+    free(buffer);
 
     return 0;
 }
